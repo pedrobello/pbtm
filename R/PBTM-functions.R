42d6f0982d2f7c73adc96df52153512ba3e0e4c9
Pedro Bello <pedrobello@gmail.com>
2019-08-06 13:24:33 -0700

    Add Treat.desc field to Calct50 function

--- R/PBTM-functions.R
+++ R/PBTM-functions.R
@@ -9,12 +9,12 @@
 CalcT50nGR50 <- function()
 {
   # Calculate Time to 50% Germination (T50) (calculate on raw data to avoid loss of points closer to 50% germination) + GR50
-  Treatments <<- TreatData %>% group_by(Treat.ID, Treat.aging.time, Treat.priming.wp, Treat.priming.temp,Treat.priming.duration,Germ.wp,Germ.temp, Germ.promoter.dosage, Germ.inhibitor.dosage) %>%
+  Treatments <<- TreatData %>% group_by(Treat.ID, Treat.desc, Treat.aging.time, Treat.priming.wp, Treat.priming.temp,Treat.priming.duration,Germ.wp,Germ.temp, Germ.promoter.dosage, Germ.inhibitor.dosage) %>%
     dplyr::mutate(T50 = approx(Germ.fraction,Germ.time.hours, xout=0.5, ties="ordered")$y,
                   GR50 = 1/approx(Germ.fraction,Germ.time.hours, xout=0.5, ties="ordered")$y)
 
   # Separate all treatments without germination time courses
-  Treatments <<- Treatments %>% group_by(Treat.ID, Treat.aging.time, Treat.priming.wp, Treat.priming.temp,Treat.priming.duration,Germ.wp,Germ.temp, Germ.promoter.dosage, Germ.inhibitor.dosage, T50, GR50) %>% tally()
+  Treatments <<- Treatments %>% group_by(Treat.ID, Treat.desc, Treat.aging.time, Treat.priming.wp, Treat.priming.temp,Treat.priming.duration,Germ.wp,Germ.temp, Germ.promoter.dosage, Germ.inhibitor.dosage, T50, GR50) %>% tally()
 }
 
 
