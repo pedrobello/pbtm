ee6aca5e63838744fa7191706ad5a425b8244631
Pedro Bello <pedrobello@gmail.com>
2021-01-27 15:04:09 -0800

    eval back

--- R/functions.R
+++ R/functions.R
@@ -127,19 +127,19 @@ PlotRawDt <- function(Data, Treat1, Treat2, Treat3)
   if (missing(Treat1)) { #treatment not informed
     print("Informed treatment for factor.")
   } else {
-
+    eval(parse(text=paste("(as.factor(TreatData$",Treat1,"))", sep = "")))
     T1 <- Treat1
   }
   if (missing(Treat2)) { #treatment not informed
     T2 <- NA
   } else {
-  #  eval(parse(text=paste("(as.factor(TreatData$",Treat2,"))", sep = "")))
+    eval(parse(text=paste("(as.factor(TreatData$",Treat2,"))", sep = "")))
     T2 <- Treat2
   }
   if (missing(Treat3)) { #treatment not informed
     T3 <- NA
   } else {
-   # T3 <- eval(parse(text=paste("(as.factor(TreatData$",Treat3,"))", sep = "")))
+    eval(parse(text=paste("(as.factor(TreatData$",Treat3,"))", sep = "")))
     T3 <- Treat3
   }
 
