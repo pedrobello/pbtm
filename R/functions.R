ad948514150303e62125009bdb94f989ef2ebb7a
Pedro Bello <pedrobello@gmail.com>
2021-01-26 21:32:20 -0800

    rename col instead

--- R/functions.R
+++ R/functions.R
@@ -85,14 +85,17 @@ CalcTxnGRx <- function(Data, Fraction, T1ColName, T2ColName, T3ColName, T4ColNam
 
   # Calculate Time to 50% Germination (T50) (calculate on raw data to avoid loss of points closer to 50% germination) + GR50
   Treatments <- Data %>% group_by_at(TreatColNames) %>%
-    dplyr::mutate(FracSpeedLbl[1] = approx(CumFract,CumTime, xout=Frac, ties="ordered")$y,
+    dplyr::mutate(Tx = approx(CumFract,CumTime, xout=Frac, ties="ordered")$y,
                   GRx = 1/approx(CumFract,CumTime, xout=Frac, ties="ordered")$y)
 
-  print(Treatments)
 
   TreatColNames <- c(TreatColNames, "Tx", "GRx")
 
   # Separate all treatments without germination time courses
   Treatments <- Treatments %>% group_by_at(TreatColNames) %>% tally()
+
+  names(Treatments)[names(Treatments) == "Tx"] <- FracSpeedLbl
+  names(Treatments)[names(Treatments) == "GRx"] <- FracRateLbl
+
   return(Treatments)
 }
