e6fae109a028a9b35f3c94420ec0f71795baa5e8
Pedro Bello <pedrobello@gmail.com>
2021-01-28 10:28:09 -0800

    plot x scale

--- R/functions.R
+++ R/functions.R
@@ -85,6 +85,58 @@ PlotRateVsTreat <- function (Data, x, y)
   pGR
 }
 
+
+#' A Function to plot the cumulative raw data selected.
+#'
+#' This function plots the raw data previously selected. It will plot raw cumulative curves over time.
+#' @param Data with time course data for one or more treatments.
+#' @param Treat1,Treat2 are the desired treatment factors to be informed as column names. The first informed treatment will be separated as color and the second as shape.
+#' @keywords plot raw data
+#' @import ggplot2
+#' @export
+#' @examples PlotRawDt(MyData,"Germ.temp")
+#' PlotRawDt(MyRawData,"Germ.temp")
+PlotRawDt <- function(Data, Treat1, Treat2)
+{
+  TreatData <- Data
+  MaxTime <- TreatData$CumTime[which.max(TreatData$CumTime)] #Gets the longest time measurement in the dataset provided.
+  PlotTime <- ceiling(MaxTime/5)*5 #make maxTime a multiple of 5 - ceiling() rounds up
+  Increment <- round(PlotTime/5, digits = 0) #define tick mark separation
+
+  gp <- "geom_point(shape=19, size=2)" #Add standard fixed shape in case shape is not used as the second factor
+
+  if (missing(Treat1)) { #treatment 1 not informed
+    print("Informed treatment for factor.")
+  } else {
+    eval(parse(text=paste("TreatData$",Treat1, " <- (factor(TreatData$",Treat1,"))", sep = "")))
+    T1 <- Treat1
+  }
+  if (missing(Treat2)) { #treatment 2 not informed
+    T2 <- NA
+  } else {
+    gp <- "geom_point(size=2)" #Add fixed shape in case shape is not used as the second factor
+    eval(parse(text=paste("TreatData$",Treat2, " <- (factor(TreatData$",Treat2,"))", sep = "")))
+    T2 <- Treat2
+  }
+  #if (missing(Treat3)) { #treatment 3 not informed - possible in the near future
+  #  T3 <- NA
+  #  alph <- ""
+  #} else {
+  # eval(parse(text=paste("(as.factor(TreatData$",Treat3,"))", sep = "")))
+  #  T3 <- Treat3
+  #  alph <- "color=T1, shape=T2, alpha=T3"
+  #}
+
+  #Plot All Treatments with fitted equation (Whole data plot here, including repetitive percentages)
+  pRaw <<- ggplot(data=TreatData, aes_string(x="CumTime", y="CumFract", color=T1, shape=T2 )) +
+    eval(parse(text=gp)) + geom_line() + xlab("Time") + ylab("Cumulative (%)") +
+    scale_y_continuous(labels = scales::percent, expand = c(0,0), limits = c(0,1.02)) +
+    scale_x_continuous(expand = c(0,0), limits = c(0,PlotTime)) +
+    theme_scatter_plot
+  pRaw
+}
+
+
 #ggplot package theme ----------------------------------------------------------------------------------
 theme_scatter_plot <- theme(
   legend.background = element_blank(),
@@ -107,55 +159,7 @@ theme_scatter_plot <- theme(
 
 #----------------------New Development - Under Testing
 
-#' A Function to plot the cumulative raw data selected.
-#'
-#' This function plots the raw data previously selected. It will plot raw cumulative curves over time.
-#' @param Data with time course data for one or more treatments.
-#' @param Treat1,Treat2,Treat3 are the desired treatment factors to be informed as column names. The first informed treatment will be separated as color, the second as shape and third as alpha (transparency).
-#' @keywords plot raw data
-#' @import ggplot2
-#' @export
-#' @examples PlotRawDt(MyData)
-#' PlotRawDt(MyRawData)
-PlotRawDt <- function(Data, Treat1, Treat2)
-{
-  TreatData <- Data
-  MaxTime <- TreatData$CumTime[which.max(TreatData$CumTime)] #Gets the longest time measurement in the dataset provided.
-  PlotTime <- ceiling(MaxTime/5)*5 #make maxTime a multiple of 5
-  Increment <- round(PlotTime/5, digits = 0) #define tick mark separation
 
-  gp <- "geom_point(shape=19, size=2)" #Add standard fixed shape in case shape is not used as the second factor
-
-  if (missing(Treat1)) { #treatment 1 not informed
-    print("Informed treatment for factor.")
-  } else {
-    eval(parse(text=paste("TreatData$",Treat1, " <- (factor(TreatData$",Treat1,"))", sep = "")))
-    T1 <- Treat1
-  }
-  if (missing(Treat2)) { #treatment 2 not informed
-    T2 <- NA
-  } else {
-    gp <- "geom_point(size=2)" #Add fixed shape in case shape is not used as the second factor
-    eval(parse(text=paste("TreatData$",Treat2, " <- (factor(TreatData$",Treat2,"))", sep = "")))
-    T2 <- Treat2
-  }
-  #if (missing(Treat3)) { #treatment 3 not informed - possible in the near future
-  #  T3 <- NA
-  #  alph <- ""
-  #} else {
-    # eval(parse(text=paste("(as.factor(TreatData$",Treat3,"))", sep = "")))
-  #  T3 <- Treat3
-  #  alph <- "color=T1, shape=T2, alpha=T3"
-  #}
-
-  #Plot All Treatments with fitted equation (Whole data plot here, including repetitive percentages)
-  pRaw <<- ggplot(data=TreatData, aes_string(x="CumTime", y="CumFract", color=T1, shape=T2 )) +
-    eval(parse(text=gp)) + geom_line() + xlab("Time") + ylab("Cumulative (%)") +
-    scale_y_continuous(labels = scales::percent, expand = c(0,0), limits = c(0,1.02)) +
-    scale_x_continuous(expand = c(0,0), limits = c(0,PlotTime+(Increment/5))) +
-    theme_scatter_plot
-  pRaw
-}
 
 
 
