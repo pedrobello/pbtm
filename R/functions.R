ef0dbd57124e7e4da3056e58f96d658b55bf7c51
Pedro Bello <pedrobello@gmail.com>
2021-01-27 22:52:59 -0800

    gp

--- R/functions.R
+++ R/functions.R
@@ -129,10 +129,11 @@ PlotRawDt <- function(Data, Treat1, Treat2, Treat3)
   } else {
     eval(parse(text=paste("TreatData$",Treat1, " <- (factor(TreatData$",Treat1,"))", sep = "")))
     T1 <- Treat1
+    gp <- "geom_point(size=2)"
   }
   if (missing(Treat2)) { #treatment not informed
     T2 <- NA
-    addshape <- "shape=19"
+    gp <- "geom_point(shape=19, size=2)"
   } else {
     eval(parse(text=paste("TreatData$",Treat2, " <- (factor(TreatData$",Treat2,"))", sep = "")))
     T2 <- Treat2
@@ -146,9 +147,11 @@ PlotRawDt <- function(Data, Treat1, Treat2, Treat3)
     alph <- "alpha=T3"
   }
 
+#  If T2 not assigned ... shape=19
+
   #Plot All Treatments with fitted equation (Whole data plot here, including repetitive percentages)
-  pRaw <<- ggplot(data=TreatData, aes_string(x="CumTime", y="CumFract", color=T1, shape=T2, eval(parse(text=alph)) )) +
-    geom_point(parse(addshape), size=2) + geom_line() + xlab("Time") + ylab("Cumulative (%)") +
+  pRaw <<- ggplot(data=TreatData, aes_string(x="CumTime", y="CumFract", color=T1, shape=T2)) +
+    eval(parse(text=gp)) + geom_line() + xlab("Time") + ylab("Cumulative (%)") +
     scale_y_continuous(labels = scales::percent, expand = c(0,0), limits = c(0,1.02)) +
     scale_x_continuous(expand = c(0,0), limits = c(0,PlotTime+(Increment/5))) +
     theme_scatter_plot
