b03a103a3b0f3922d1a7822be969b7368cd06336
Pedro Bello <pedrobello@gmail.com>
2021-01-28 11:52:40 -0800

    CleanData

--- R/functions.R
+++ R/functions.R
@@ -5,7 +5,7 @@
 #' This function allows you to calculate the time to a desired cumulative fraction and respective germination rate. Use this function on raw data to avoid loss of points closer to the desired cumulative fraction.
 #' @param Data time course and cumulative dataset. Several treatments can be used at once as long as it respects the template and column names provided. A column with time in hours (Germ.time.hours) + a column with cumulative fractions (Germ.fraction) are required with at least one additional column for relevant treatment (e.g., germination temperature or water potential)
 #' @param Fraction from 0 to 1 used to calculate the time required for that level to be obtained in the cumulative time course. Standard value is 0.5 (50 percent), to calculate the time to 50 percent germination (T50) and respective germination rate (GR50). Fraction level can be entered and be used for calculation and change column name.
-#' @param T1ColName,T2ColName,T3ColName,T4ColName,T5ColName are the names of the treatment columns to separate the dataset. The time course cumulative curves will be grouped for each distinct treatment that should be informed here. These column names do not need to be informed in case the provided template file is used to organize the data.
+#' @param Treat1,Treat2,Treat3,Treat4,Treat5 are the names of the treatment columns to separate the dataset. The time course cumulative curves will be grouped for each distinct treatment that should be informed here. These column names do not need to be informed in case the provided template file is used to organize the data.
 #' @keywords Tx, GRx, germination speed, germination rate
 #' @importFrom dplyr group_by_at
 #' @importFrom dplyr tally
@@ -13,20 +13,20 @@
 #' @export
 #' @examples
 #' CalcSpeed(MyData)
-CalcSpeed <- function(Data, Fraction, T1ColName, T2ColName, T3ColName, T4ColName, T5ColName)
+CalcSpeed <- function(Data, Fraction, Treat1, Treat2, Treat3, Treat4, Treat5)
 {
   #Define the informed treatments (columns) to be grouped when calling the function or set standard to all columns besides CumFract and CumTime.
-  if (missing(T5ColName)) { #T5ColName not informed
-    if (missing(T4ColName)) { #T4ColName not informed
-      if (missing(T3ColName)) { #T3ColName not informed
-        if (missing(T2ColName)) { #T2ColName not informed
-          if (missing(T1ColName)) { #T1ColName not informed
+  if (missing(Treat5)) { #Treat5 not informed
+    if (missing(Treat4)) { #Treat4 not informed
+      if (missing(Treat3)) { #Treat3 not informed
+        if (missing(Treat2)) { #Treat2 not informed
+          if (missing(Treat1)) { #Treat1 not informed
             TreatColNames <- c("Treat.ID","Treat.desc","Treat.aging.time","Treat.priming.wp","Treat.priming.temp","Treat.priming.duration","Germ.wp","Germ.temp", "Germ.promoter.dosage", "Germ.inhibitor.dosage")
-          } else {TreatColNames <- c(T1ColName)}
-        } else {TreatColNames <- c(T1ColName,T2ColName)}
-      } else {TreatColNames <- c(T1ColName,T2ColName,T3ColName)}
-    } else {TreatColNames <- c(T1ColName,T2ColName,T3ColName,T4ColName)}
-  } else {TreatColNames <- c(T1ColName,T2ColName,T3ColName,T4ColName,T5ColName)}
+          } else {TreatColNames <- c(Treat1)}
+        } else {TreatColNames <- c(Treat1,Treat2)}
+      } else {TreatColNames <- c(Treat1,Treat2,Treat3)}
+    } else {TreatColNames <- c(Treat1,Treat2,Treat3,Treat4)}
+  } else {TreatColNames <- c(Treat1,Treat2,Treat3,Treat4,Treat5)}
 
   #Define the informed fraction for time and rate calculation. Standard is 0.5 -> 50 percent. Also populates new column name base in the fraction used.
   if (missing(Fraction)) { #Fraction not informed
@@ -55,6 +55,7 @@ CalcSpeed <- function(Data, Fraction, T1ColName, T2ColName, T3ColName, T4ColName
 
   return(Treatments)
 }
+
 
 #' A Function to plot rate vs desired treatment.
 #'
@@ -99,9 +100,9 @@ PlotRateVsTreat <- function (Data, x, y)
 PlotRawDt <- function(Data, Treat1, Treat2)
 {
   TreatData <- Data
-  MaxTime <- TreatData$CumTime[which.max(TreatData$CumTime)] #Gets the longest time measurement in the dataset provided.
-  PlotTime <- ceiling(MaxTime/5)*5 #make maxTime a multiple of 5 - ceiling() rounds up
-  Increment <- round(PlotTime/5, digits = 0) #define tick mark separation
+  #MaxTime <- TreatData$CumTime[which.max(TreatData$CumTime)] #Gets the longest time measurement in the dataset provided.
+  #PlotTime <- ceiling(MaxTime/5)*5 #make maxTime a multiple of 5 - ceiling() rounds up
+  #Increment <- round(PlotTime/5, digits = 0) #define tick mark separation
 
   gp <- "geom_point(shape=19, size=2)" #Add standard fixed shape in case shape is not used as the second factor
 
@@ -160,6 +161,58 @@ theme_scatter_plot <- theme(
 #----------------------New Development - Under Testing
 
 
+#' A Function to clean cumulative curves on dataset.
+#'
+#' This function removes repetitive cumulative fractions/percentages, keeping only the initial presence of the value
+#' @param Data object with the raw cumulative data that needs to be removed.
+#' @param Treat1,Treat2,Treat3,Treat4,Treat5 are the names of the treatment columns to separate the dataset. The time course cumulative curves will be grouped for each distinct treatment that should be informed here.
+#' @keywords Clean cumulative fraction repetitive percentage
+#' @importFrom dplyr distinct
+#' @export
+#' @examples CleanData(mydata,"Treat.ID")
+#' CleanData(mydata,"Treat.ID")
+CleanData <- function(Data, Treat1, Treat2, Treat3, Treat4, Treat5) {
+  #Clean Repetitive Percentages (keeps only initial presence of value)
 
+  if (missing(Data)) { #data object not informed
+    print("Informe the data object.")
+  } else {
+  TreatData <- Data
+  if (missing(Treat1)) { #treatment 1 not informed
+    print("Informed treatment for factor.")
+  } else {
+    T1 <-  Treat1
+    if (missing(Treat2)) { #treatment 2 not informed
+      T2 <- ""
+    } else {
+      T2 <- paste(",",Treat2, sep = "")
+    }
+    if (missing(Treat3)) { #treatment 3 not informed
+      T3 <- ""
+    } else {
+      T3 <- paste(",",Treat3, sep = "")
+    }
+    if (missing(Treat4)) { #treatment 2 not informed
+      T4 <- ""
+    } else {
+      T4 <- paste(",",Treat4, sep = "")
+    }
+    if (missing(Treat5)) { #treatment 2 not informed
+      T5 <- ""
+    } else {
+      T5 <- paste(",",Treat5, sep = "")
+    }
 
+    TreatDataClean <- eval(parse(text=paste("distinct(TreatData,",T1,T2,T3,T4,T5,",CumFract, .keep_all = TRUE)", sep="")))
+
+    #eval(parse(text=paste("TreatData$",Treat1, " <- (factor(TreatData$",Treat1,"))", sep = "")))
+
+    #eval(parse(text=paste("TreatData$",Treat1, " <- (factor(TreatData$",Treat1,"))", sep = "")))
+
+    #TreatDataClean <- distinct(TreatData, Treat.ID, Germ.temp, Germ.wp, CumFract, .keep_all = TRUE)
+
+    return(TreatDataClean)
+  }
+  }
+}
 
