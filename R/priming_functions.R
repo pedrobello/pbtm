70c45e56738e1a4e86ef9b9f6292d8b158ca1c10
Ben Bradford <bradfordium@gmail.com>
2022-01-22 16:59:48 -0600

    update priming models

--- R/priming_functions.R
+++ R/priming_functions.R
@@ -1,39 +1,68 @@
-#-----------------Tested Functions - MAc OS and Windows
-#
 #' A Function to calculate the Hydropriming model parameters.
 #'
 #' This function calculates the minimal water potential for priming effects (Psib50min).
-#' @param Data object with the calculated rates with treatments to be used in the Hydropriming model. The output of the CalcSpeed function can be directly used here with the desired treatments. The fields with Treat.priming.wp and Treat.priming.duration need to be informed in the data file.
-#' @param GR is the column name for the rate to be used in the model and needs to be informed in case the data object contains a GR different than GR50.
+#' @param data Data rame with the calculated rates with treatments to be used in the Hydropriming model. The output of the calcSpeed function can be directly used here with the desired treatments. Priming water potential and priming duration treatments are required.
+#' @param priming.wp Column containing the priming water potential treatments.
+#' @param priming.duration Column containing the priming duration treatments.
+#' @param rate Column containing the growth rate data from the calcSpeed function.
 #' @keywords hydropriming model parameters
 #' @export
-#' @examples CalcHPModel(MyData, "GR20")
-#' CalcHPModel(MyData)
-CalcHPModel <- function(Data, GR)
-{
-  Treatments <- Data
-  if (missing(GR)) { #GR not informed
-    grColName <- "GR50"
-  } else {
-    grColName <- GR
-  }
+#' @examples calcHPModel(MySpeedData)
+#' calcHPModel(MySpeedData)
 
-  #Initilaze values for model calculation
+calcHPModel <- function(data, priming.wp = "PrimingWP", priming.duration = "PrimingDuration", rate = "GR50") {
+
+  # data and argument checks
+  if (!is.data.frame(data)) stop("Data is not a valid data frame.")
+
+  # check validity of columns defs
+  if (!is.element(priming.wp, names(data))) stop("Priming water potential column '", priming.wp, "' not found in data frame.")
+  if (!is.element(priming.duration, names(data))) stop("Priming duration column '", priming.duration, "' not found in data frame.")
+  if (!is.element(rate, names(data))) stop("Growth rate column '", rate, "' not found in data frame.")
+
+  primingWP <- data[[priming.wp]]
+  primingDuration <- data[[priming.duration]]
+  growthRate <- data[[rate]]
+
+  # initialize values for model calculation
   psiMin50 <- -1
-  grUsed <- eval(parse(text=paste("Treatments$",grColName, sep = ""))) #grUsed <- PrimingTreats$GR50
-  Pwp <- Treatments$Treat.priming.wp #Pwp <- PrimingTreats$Treat.priming.wp
-  Pdur <- Treatments$Treat.priming.duration #Pdur <- PrimingTreats$Treat.priming.duration
-
-  #Function to calculate Theta Hydro Priming
-  fthetaHP <- function(PM50){(Pwp-PM50)*Pdur}
 
   # HydroPriming - Calculate PsiMin50, y intercept and slope using NLS
-  GetPsiMin50 <- nls(grUsed ~ inTer + fthetaHP(psiMin50) * sLope, algorithm="port",
-                     start=c(inTer=0.001,psiMin50=-1,sLope=0.1),lower=c(inTer=0.0000001,psiMin50=-10, sLope=0.000000001),upper=c(inTer=0.1,psiMin50=-1, sLope=1))
+  nls_model <- stats::nls(
+    formula = growthRate ~ intercept + slope * ((primingWP - psiMin50) * primingDuration),
+    start = list(
+      intercept = 0.001,
+      psiMin50 = -1,
+      slope = 0.1),
+    lower = list(
+      intercept = 0.0000001,
+      psiMin50 = -10,
+      slope = 0.000000001),
+    upper = list(
+      intercept = 0.1,
+      psiMin50 = -1,
+      slope = 1),
+    algorithm = "port")
 
-  PsiMin50 <- round(summary(GetPsiMin50)$coefficients[[2]],3) # PsiMin50 Value
-  Intercept <- round(summary(GetPsiMin50)$coefficients[[1]],4) # Intercept
-  Slope <- round(summary(GetPsiMin50)$coefficients[[3]],6) # Slope
+  Intercept <- summary(nls_model)$coefficients[[1]]
+  PsiMin50 <- summary(nls_model)$coefficients[[2]]
+  Slope <- summary(nls_model)$coefficients[[3]]
+
+  message("HydroPriming nonlinear least-squares model summary:")
+  show(summary(nls_model))
+
+  #Hydropriming model linear regression
+  l_model <- stats::lm(
+    formula = growthRate ~ (primingWP - psiMin50) * primingDuration,
+    data = tibble(
+      growthRate = growthRate,
+      primingWP = primingWP,
+      psiMin50 = PsiMin50,
+      primingDuration = primingDuration
+    ))
+
+  message("HydroPriming linear model summary:")
+  show(summary(l_model))
 
   #Future refine of the model
   #lmer(GR50 ~ ((Treat.priming.wp-psiMin50)*Treat.priming.duration), PrimingTreats, start = c(psiMin50 = -1) )
@@ -41,72 +70,111 @@ CalcHPModel <- function(Data, GR)
   #HPlModel <- lm(GR50 ~ fthetaHP(PsiMin50,Treat.priming.wp,Treat.priming.duration), PrimingTreats)
   #summary(HPlModel)
 
-  #Hydropriming model linear regression
-  HPlModel <- lm(grUsed ~ fthetaHP(PsiMin50))
-
   #get some estimation of goodness of fit
-  Correlation <- cor(grUsed,predict(HPlModel))^2
-  RSquared <- round(Correlation[1],2)
+  corr <- stats::cor(growthRate, stats::predict(l_model)) ^ 2
+  RSquared <- round(corr[1], 2)
 
-  Model <- "HP"
-  HPModelResults <- data.frame(Model,PsiMin50,Intercept,Slope,RSquared)
-  return(HPModelResults)
+  results <- list(
+    Type = "HydroPriming",
+    Model = nls_model,
+    LinearModel = l_model,
+    Rate = rate,
+    PsiMin50 = PsiMin50,
+    Intercept = Intercept,
+    Slope = Slope,
+    RSquared = RSquared)
+
+  return(results)
 }
 
 
 #' A Function to calculate the Hydrothermal priming model parameters.
 #'
 #' This function calculates the minimal water potential for priming effects (Psibmin50) and minimal temperature (Tmin).
-#' @param myData object with the calculated rates with treatments to be used in the Hydrothermal priming model. The output of the CalcSpeed function can be directly used here with the desired treatments. The fields with Treat.priming.wp, Treat.priming.temp and Treat.priming.duration need to be informed in the data file.
-#' @param GR is the column name for the rate to be used in the model and needs to be informed in case the data object contains a GR different than GR50.
+#' @param data object with the calculated rates with treatments to be used in the Hydrothermal priming model. The output of the CalcSpeed function can be directly used here with the desired treatments. The fields with Treat.priming.wp, Treat.priming.temp and Treat.priming.duration need to be informed in the data file.
+#' @param priming.wp Column containing the priming water potential treatments.
+#' @param priming.temp Column containing the priming temperature treatments.
+#' @param priming.duration Column containing the priming duration treatments.
+#' @param rate Column containing the growth rate calculations from the calcSpeed function.
 #' @keywords hydrothermal priming model parameters
 #' @export
-#' @examples CalcHTPModel(myData, "GR90")
-#' CalcHTPModel(myData)
-CalcHTPModel <- function(Data, GR)
-{
-  Treatments <- Data
-  if (missing(GR)) { #GR not informed
-    grColName <- "GR50"
-  } else {
-    grColName <- GR
-  }
-  #Initilaze values for model calculation
-  psiMin50i <- -1
-  Tmini <- 12
-  grUsed <- eval(parse(text=paste("Treatments$",grColName, sep = ""))) #grUsed <- PrimingTreats$GR50
-  Pwp <- Treatments$Treat.priming.wp
-  Ptemp <- Treatments$Treat.priming.temp
-  Pdur <- Treatments$Treat.priming.duration
+#' @examples calcHTPModel(MySpeedData)
+#' calcHTPModel(MySpeedData)
 
-  #Function to calculate Theta Hydro Priming
-  fthetaHTP <- function(PM50,TMIN){(Pwp-PM50)*(Ptemp-TMIN)*Pdur}
+calcHTPModel <- function(data, priming.wp = "PrimingWP", priming.temp = "PrimingTemp", priming.duration = "PrimingDuration", rate = "GR50") {
+
+  # data and argument checks
+  if (!is.data.frame(data)) stop("Data is not a valid data frame.")
+
+  # check validity of columns defs
+  if (!is.element(priming.wp, names(data))) stop("Priming water potential column '", priming.wp, "' not found in data frame.")
+  if (!is.element(priming.temp, names(data))) stop("Priming temperature column '", priming.temp, "' not found in data frame.")
+  if (!is.element(priming.duration, names(data))) stop("Priming duration column '", priming.duration, "' not found in data frame.")
+  if (!is.element(rate, names(data))) stop("Growth rate column '", rate, "' not found in data frame.")
+
+  primingWP <- data[[priming.wp]]
+  primingTemp <- data[[priming.temp]]
+  primingDuration <- data[[priming.duration]]
+  growthRate <- data[[rate]]
 
   # HydroPriming - Calculate PsiMin50, y intercept and slope using NLS
-  GetPsiMin50Tmin <- nls(grUsed ~ inTer + fthetaHTP(psiMin50,Tmin) * sLope, algorithm="port",
-                         start=c(inTer=0.001,psiMin50=psiMin50i,Tmin=Tmini,sLope=0.1),lower=c(inTer=0.0000001,psiMin50=-10,Tmin=0.5,sLope=0.000000001),upper=c(inTer=0.1,psiMin50=-1,Tmin=20,sLope=1))
+  nls_model <- stats::nls(
+    formula = growthRate ~ intercept + slope * ((primingWP - psiMin50) * (primingTemp - tMin) * primingDuration),
+    start = list(
+      intercept = 0.001,
+      psiMin50 = -1,
+      tMin = 12,
+      slope = 0.1),
+    lower = list(
+      intercept = 0.0000001,
+      psiMin50 = -10,
+      tMmin = 0.5,
+      slope = 0.000000001),
+    upper = list(
+      intercept = 0.1,
+      psiMin50 = -1,
+      tMin = 20,
+      slope = 1),
+    algorithm = "port")
 
-  PsiMin50 <- round(summary(GetPsiMin50Tmin)$coefficients[[2]],3) # PsiMin50 Value
-  Tmin <- round(summary(GetPsiMin50Tmin)$coefficients[[3]],3) # Tmin Value
-  Intercept <- round(summary(GetPsiMin50Tmin)$coefficients[[1]],4) # Intercept
-  Slope <- round(summary(GetPsiMin50Tmin)$coefficients[[4]],6) # Slope
+  message("HydroThermalPriming nonlinear least-squares model summary:")
+  show(summary(nls_model))
 
-  #Hydropriming model linear regression
-  HTPlModel <- lm(grUsed ~ fthetaHTP(PsiMin50,Tmin))
+  Intercept <- summary(nls_model)$coefficients[[1]]
+  PsiMin50 <- summary(nls_model)$coefficients[[2]]
+  TMin <- summary(nls_model)$coefficients[[3]]
+  Slope <- summary(nls_model)$coefficients[[4]]
 
-  #get some estimation of goodness of fit
-  Correlation <- cor(grUsed,predict(HTPlModel))^2
-  RSquared <- round(Correlation[1],2)
+  # Hydropriming model linear regression
+  l_model <- stats::lm(
+    formula = growthRate ~ ((primingWP - psiMin50) * (primingTemp - tMin) * primingDuration),
+    data = tibble(
+      growthRate = growthRate,
+      primingWP = primingWP,
+      psiMin50 = PsiMin50,
+      primingTemp = primingTemp,
+      tMin = TMin,
+      primingDuration = primingDuration
+    ))
+
+  message("HydroThermalPriming linear model summary:")
+  show(summary(l_model))
+
+  # get some estimation of goodness of fit
+  corr <- stats::cor(growthRate, stats::predict(l_model)) ^ 2
+  RSquared <- round(corr[1], 2)
 
   Model <- "HTP"
-  HTPModelResults <- data.frame(Model,PsiMin50,Tmin,Intercept,Slope,RSquared)
-  return(HTPModelResults)
+  results <- list(
+    Type = "HydroThermalPriming",
+    Model = nls_model,
+    LinearModel = l_model,
+    Rate = rate,
+    PsiMin50 = PsiMin50,
+    Tmin = TMin,
+    Intercept = Intercept,
+    Slope = Slope,
+    RSquared = RSquared)
+
+  return(results)
 }
-
-
-
-#----------------------New Development - Under Testing
-
-
-
-
